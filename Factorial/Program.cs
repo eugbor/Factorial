e9f6d1a3b5f8ead3e8ec1c77f2f8be4c6934ef08
eugborn <eugeneborodin@mail.ru>
2016-05-18 17:02:04 +0300

    init

---
+++ Factorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.Write("Enter n: ");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Factorial n! = {0}", fact(c));
+            Console.ReadKey();
+        }
+
+        public static int fact(int n)
+        {
+            return (n == 0) ? 1 : n * fact(n - 1);
+        }
+
+    }
+}
