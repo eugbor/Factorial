97e9400c95fae3071826b30c34a07fcb17fe5e57
eugborn <eugeneborodin@mail.ru>
2016-06-25 14:00:42 +0300

    commit

--- Factorial/Program.cs
+++ Factorial/Program.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace Factorial
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
 
         {
             Console.Write("Enter n: ");
@@ -22,5 +22,14 @@ namespace Factorial
             return (n == 0) ? 1 : n * fact(n - 1);
         }
 
+        //public static int fact(int a)
+        //{
+        //    int c = 1;
+        //    for (int i = 1; i <= a; i++)
+        //    {
+        //        c = c * i;
+        //    }
+        //    return c;
+        //}
     }
 }
